8ea66e35211baf42ff218f51d220fdddeb93b923
Novello <josesergio.novello@thomsonreuters.com>
2026-03-31 13:08:07 -0300

    Update site

---
+++ src/pages/diarias.tsx
@@ -0,0 +1,7 @@
+export default function Diarias() {
+  return (
+    <div className="min-h-screen p-8 max-w-xl space-y-6">
+      <h1 className="text-3xl font-bold">WYD Help</h1>
+    </div>
+  );
+}
