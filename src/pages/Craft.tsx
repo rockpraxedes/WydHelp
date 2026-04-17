eb6ef099a85e2789ad59f6054f72a80a668057cb
Novello <josesergio.novello@thomsonreuters.com>
2026-04-17 11:48:30 -0300

    nome botao

--- src/pages/Craft.tsx
+++ src/pages/Craft.tsx
@@ -800,7 +800,7 @@ export function Craft() {
                 color: accent,
               }}
             >
-              <span>📦</span> Itens
+              <span>📦</span> Itens Necessários
             </button>
           </div>
 
